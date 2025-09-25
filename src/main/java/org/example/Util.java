e5eefc93a14a546bd6e0e07599894d3e411cf77b
Tompi-dev <tomiris.kassymova.98@gmail.com>
2025-09-26 02:35:43 +0500

    refactor(util): partition, swap, shuffle, guards

---
+++ src/main/java/org/example/Util.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import java.util.Random;
+
+public class Util {
+    private static final Random rand = new Random();
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+
+    public static int partition(int[] arr, int low, int high, int pivotValue) {
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivotValue) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+
+    public static int randomPivot(int low, int high) {
+        return low + rand.nextInt(high - low + 1);
+    }
+
+
+    public static void shuffle(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            int j = rand.nextInt(i + 1);
+            swap(arr, i, j);
+        }
+    }
+
+
+    public static boolean isSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) return false;
+        }
+        return true;
+    }
+}
